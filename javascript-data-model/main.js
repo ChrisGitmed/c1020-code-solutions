51c28846b53c5e4aa7d0291a866c3d0e6487dba1
Chris Gitmed <cgitmed@gmail.com>
2020-10-13 10:57:40 -0700

    Revised JavaScript - Data Model

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -6,9 +6,11 @@ var orderHistory = [
     dateOrderPlaced: '08/04/20',
     priceTotal: '34.00',
     shipTo: 'JS Masher',
-    titleOfItem: 'JavaScript for impatient programmers',
+    titleOfItem: 'JavaScript for Impatient Programmers',
+    image: URL(),
     author: 'Rauschmayer, Dr. Axel',
-    returnWindowClosed: '09/07/20'
+    returnWindowClosed: '09/07/20',
+    subtotal: '31.55'
   },
   {
     orderNumber: '113-9984268-1280257',
@@ -16,17 +18,21 @@ var orderHistory = [
     priceTotal: '44.53',
     shipTo: 'JS Masher',
     titleOfItem: 'The Timeless Way of Building',
+    image: URL(),
     author: 'Alexander, Christopher',
-    returnWindowClosed: '08/19/20'
+    returnWindowClosed: '08/19/20',
+    subtotal: '41.33'
   },
   {
     orderNumber: '114-2875557-9059409',
     dateOrderPlaced: '07/04/20',
-    priceTotal: '15.98',
+    priceTotal: '17.22',
     shipTo: 'JS Masher',
-    titleOfItem: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii UIEvent, PC, Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    titleOfItem: ['Gamecube Controller Adapter', 'Super Smash Bros Switch Gamecube Adapter for Wii U', 'PC', 'Support Turbo and Vibration Features', 'No Driver and No Lag-Gamecube Adapter'],
+    image: URL(),
     author: '',
-    returnWindowClosed: '08/05/20'
+    returnWindowClosed: '08/05/20',
+    subtotal: '15.98'
   },
   {
     orderNumber: '113-2883177-2648248',
@@ -34,7 +40,9 @@ var orderHistory = [
     priceTotal: '138.93',
     shipTo: 'JS Masher',
     titleOfItem: ['Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch', 'The Art of SQL'],
+    image: [URL(), URL()],
     author: ['', 'Faroult, Stephane'],
-    returnWindowClosed: ['08/04/20', '08/04/20']
+    returnWindowClosed: ['08/04/20', '08/04/20'],
+    subtotal: ['94.95', '33.99']
   }
 ];
