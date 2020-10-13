a8fa4b7fc6a9deeafd69de6cc9c16f79898755f6
Chris Gitmed <cgitmed@gmail.com>
2020-10-13 11:38:51 -0700

    Revised JavaScript - Data Model

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -2,55 +2,61 @@
 
 var orderHistory = [
   {
-    orderNumber: '114-3941689-8772232',
-    dateOrderPlaced: '08/04/20',
-    priceTotal: '34.00',
+    orderNumber: [
+      '114-3941689-8772232',
+      '113-9984268-1280257',
+      '114-2875557-9059409',
+      '113-2883177-2648248'
+    ],
+
+    dateOrderPlaced: [
+      '08/04/20',
+      '07/19/20',
+      '07/04/20',
+      '07/03/20'
+    ],
+
+    priceTotal: [
+      '34.00',
+      '44.53',
+      '17.22',
+      '138.93'],
+
     shipTo: 'JS Masher',
-    purchasedStuff: {
-      titleOfItem: 'JavaScript for Impatient Programmers',
-      image: URL(),
-      author: 'Rauschmayer, Dr. Axel',
-      returnWindowClosed: '09/07/20',
-      subtotal: '31.55'
-    }
-  },
-  {
-    orderNumber: '113-9984268-1280257',
-    dateOrderPlaced: '07/19/20',
-    priceTotal: '44.53',
-    shipTo: 'JS Masher',
-    purchasedStuff: {
-      titleOfItem: 'The Timeless Way of Building',
-      image: URL(),
-      author: 'Alexander, Christopher',
-      returnWindowClosed: '08/19/20',
-      subtotal: '41.33'
-    }
-  },
-  {
-    orderNumber: '114-2875557-9059409',
-    dateOrderPlaced: '07/04/20',
-    priceTotal: '17.22',
-    shipTo: 'JS Masher',
-    purchasedStuff: {
-      titleOfItem: ['Gamecube Controller Adapter', 'Super Smash Bros Switch Gamecube Adapter for Wii U', 'PC', 'Support Turbo and Vibration Features', 'No Driver and No Lag-Gamecube Adapter'],
-      image: URL(),
-      author: '',
-      returnWindowClosed: '08/05/20',
-      subtotal: '15.98'
-    }
-  },
-  {
-    orderNumber: '113-2883177-2648248',
-    dateOrderPlaced: '07/03/20',
-    priceTotal: '138.93',
-    shipTo: 'JS Masher',
-    purchasedStuff: {
-      titleOfItem: ['Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch', 'The Art of SQL'],
-      image: [URL(), URL()],
-      author: ['', 'Faroult, Stephane'],
-      returnWindowClosed: ['08/04/20', '08/04/20'],
-      subtotal: ['94.95', '33.99']
-    }
+
+    titleOfItem: [
+      'JavaScript for Impatient Programmers',
+      'The Timeless Way of Building',
+      'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii U, PC, Support Turbo and Vibration Features, No Driver and No Lag-Gamecube Adapter',
+      ['Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch', 'The Art of SQL']
+    ],
+
+    image: [
+      URL(),
+      URL(),
+      URL(),
+      [URL(), URL()]
+    ],
+
+    author: [
+      'Rauschmayer, Dr. Axel',
+      'Alexander, Christopher',
+      '',
+      ['', 'Faroult, Stephane']
+    ],
+
+    returnWindowClosed: [
+      '09/07/20',
+      '08/19/20',
+      '08/05/20',
+      ['08/04/20', '08/04/20']
+    ],
+
+    subtotal: [
+      '31.55',
+      '41.33',
+      '15.98',
+      ['94.95', '33.99']
+    ]
   }
 ];
