457597cd486d2a03259d921c14ed0c0aa8c46f28
Chris Gitmed <cgitmed@gmail.com>
2020-10-13 10:35:08 -0700

    Completed JavaScript - Data Model

---
+++ javascript-data-model/main.js
@@ -0,0 +1,40 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    dateOrderPlaced: '08/04/20',
+    priceTotal: '34.00',
+    shipTo: 'JS Masher',
+    titleOfItem: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnWindowClosed: '09/07/20'
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    dateOrderPlaced: '07/19/20',
+    priceTotal: '44.53',
+    shipTo: 'JS Masher',
+    titleOfItem: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnWindowClosed: '08/19/20'
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    dateOrderPlaced: '07/04/20',
+    priceTotal: '15.98',
+    shipTo: 'JS Masher',
+    titleOfItem: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for Wii UIEvent, PC, Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: '',
+    returnWindowClosed: '08/05/20'
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    dateOrderPlaced: '07/03/20',
+    priceTotal: '138.93',
+    shipTo: 'JS Masher',
+    titleOfItem: ['Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch', 'The Art of SQL'],
+    author: ['', 'Faroult, Stephane'],
+    returnWindowClosed: ['08/04/20', '08/04/20']
+  }
+];
