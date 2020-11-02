0b3899803cc06172c38b69da660a1aa030db29b1
Chris Gitmed <cgitmed@gmail.com>
2020-11-02 10:19:35 -0800

    Completed JavaScript - Custom Methods

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,38 @@
+/* exported calculator */
+var calculator = {
+  add(x, y) {
+    return x + y;
+  },
+
+  subtract(x, y) {
+    return x - y;
+  },
+
+  multiply(x, y) {
+    return x * y;
+  },
+
+  divide(x, y) {
+    return x / y;
+  },
+
+  square(x) {
+    return x * x;
+  },
+
+  sumAll(numbers) {
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  },
+
+  getAverage(numbers) {
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum / numbers.length;
+  }
+};
