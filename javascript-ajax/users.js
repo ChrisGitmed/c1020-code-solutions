7ef2e1e9cbf3154b55785730ce64af04f5d0322a
Chris Gitmed <cgitmed@gmail.com>
2020-11-07 00:48:13 +0000

    Revised. Encapsulated in function.

--- javascript-ajax/users.js
+++ javascript-ajax/users.js
@@ -1,17 +1,21 @@
 var $userList = document.querySelector('#user-list');
 console.log('$userList: ', $userList);
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
-xhr.responseType = 'json';
-xhr.addEventListener('load', function () {
-  console.log(xhr.status);
-  console.log(xhr.response);
+function getUserData() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
 
-  for (var i = 0; i < xhr.response.length; i++) {
-    var newUser = document.createElement('li');
-    newUser.textContent = xhr.response[i].name;
-    $userList.appendChild(newUser);
-  }
-});
-xhr.send();
+    for (var i = 0; i < xhr.response.length; i++) {
+      var newUser = document.createElement('li');
+      newUser.textContent = xhr.response[i].name;
+      $userList.appendChild(newUser);
+    }
+  });
+  xhr.send();
+}
+
+getUserData();
