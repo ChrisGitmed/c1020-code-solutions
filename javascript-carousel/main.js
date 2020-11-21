d540acda5110c63520b8175dab15261378b3ee6f
Chris Gitmed <cgitmed@gmail.com>
2020-11-21 00:18:59 +0000

    Created data model for images; Revised changeImage function to use a loop.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -4,58 +4,65 @@ var $image = document.querySelector('img');
 var $dotList = document.querySelectorAll('#progress-bar > i');
 var $progressBar = document.querySelector('#progress-bar');
 
+var data = {
+  imageCounter: 0,
+  images: [
+    {
+      src: 'images/001.png',
+      alt: 'Bulbasaur'
+    },
+    {
+      src: 'images/004.png',
+      alt: 'Charmander'
+    },
+    {
+      src: 'images/007.png',
+      alt: 'Squirtle'
+    },
+    {
+      src: 'images/025.png',
+      alt: 'Pikachu'
+    },
+    {
+      src: 'images/039.png',
+      alt: 'jigglypuff'
+    }
+  ]
+};
+
 var imageIntervalID = setInterval(toggleCarouselForward, 3000);
-var imageCounter = 0;
 
 function toggleCarouselForward() {
-  if (imageCounter < 4) {
-    imageCounter++;
-  } else if (imageCounter === 4) {
-    imageCounter = 0;
+  if (data.imageCounter < 4) {
+    data.imageCounter++;
+  } else if (data.imageCounter === 4) {
+    data.imageCounter = 0;
   }
   changeImage();
 }
 
 function toggleCarouselBackward() {
-  if (imageCounter > 0) {
-    imageCounter--;
-  } else if (imageCounter === 0) {
-    imageCounter = 4;
+  if (data.imageCounter > 0) {
+    data.imageCounter--;
+  } else if (data.imageCounter === 0) {
+    data.imageCounter = 4;
   }
   changeImage();
 }
 
 function changeImage() {
-  if (imageCounter === 0) {
-    $image.src = 'images/001.png';
-    $image.alt = 'Bulbasaur';
-    toggleShading();
-
-  } else if (imageCounter === 1) {
-    $image.src = 'images/004.png';
-    $image.alt = 'Charmander';
-    toggleShading();
-
-  } else if (imageCounter === 2) {
-    $image.src = 'images/007.png';
-    $image.alt = 'Squirtle';
-    toggleShading();
-
-  } else if (imageCounter === 3) {
-    $image.src = 'images/025.png';
-    $image.alt = 'Pikachu';
-    toggleShading();
-
-  } else if (imageCounter === 4) {
-    $image.src = 'images/039.png';
-    $image.alt = 'Jigglypuff';
-    toggleShading();
+  for (var i = 0; i < data.images.length; i++) {
+    if (data.imageCounter === i) {
+      $image.src = data.images[i].src;
+      $image.alt = data.images[i].alt;
+      toggleShading();
+    }
   }
 }
 
 function toggleShading() {
   for (var i = 0; i < $dotList.length; i++) {
-    if (i === imageCounter) {
+    if (i === data.imageCounter) {
       $dotList[i].className = 'far fa-circle shaded';
     } else {
       $dotList[i].className = 'far fa-circle';
@@ -80,7 +87,7 @@ $rightNav.addEventListener('click', function (event) {
 
 $progressBar.addEventListener('click', function (event) {
   if (event.target.tagName === 'I') {
-    imageCounter = Number(event.target.dataset.id);
+    data.imageCounter = Number(event.target.dataset.id);
     changeImage();
     resetInterval();
   }
