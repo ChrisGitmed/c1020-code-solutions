164a7da297f686a17ccee2bd9e7a496e111b28ab
Chris Gitmed <cgitmed@gmail.com>
2020-12-01 17:53:46 +0000

    Revised Express - Delete

--- express-delete/index.js
+++ express-delete/index.js
@@ -2,13 +2,13 @@ const express = require('./node_modules/express');
 const app = express();
 const objectList = [
   {
-    id: 1,
+    id: 4,
     name: 'Chris',
     course: 'Express',
     grade: 9001
   },
   {
-    id: 2,
+    id: 7,
     name: 'Tim',
     course: 'Express',
     grade: 10000
@@ -19,11 +19,15 @@ app.get('/api/grades', (req, res) => {
 });
 
 app.delete('/api/grades/:id', (req, res) => {
-  objectList.splice(req.params.id);
+  for (let i = 0; i < objectList.length; i++) {
+    if (objectList[i].id === Number(req.params.id)) {
+      objectList.splice(i, 1);
+    }
+  }
   res.sendStatus(204);
 });
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
-  console.log('Server is listening!');
+  console.log('Server is listening on port 3000...');
 });
