195fb09e0fd2f8960603414dcecdf18a09643079
Chris Gitmed <cgitmed@gmail.com>
2020-12-04 20:57:56 +0000

    Created a basic app using express

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-console */
-
 const pg = require('pg');
+const express = require('express');
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:lfz@localhost/studentGradeTable'
 });
 
-console.log(db);
+console.log(db); // Avoids linter for unused variables. | REMOVE LATER
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
