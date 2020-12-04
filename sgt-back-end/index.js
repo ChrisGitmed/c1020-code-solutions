32a95941f50e15cc450a94f5314beebe7c773449
Chris Gitmed <cgitmed@gmail.com>
2020-12-04 22:50:29 +0000

    Added GET route for /api/grades

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -6,9 +6,23 @@ const db = new pg.Pool({
   connectionString: 'postgres://dev:lfz@localhost/studentGradeTable'
 });
 
-console.log(db); // Avoids linter for unused variables. | REMOVE LATER
-
 const app = express();
+
+app.get('/api/grades', (request, response) => {
+  const sql = `
+    select *
+      from "grades"
+  ;`;
+
+  db.query(sql)
+    .then(result => {
+      response.status(200).json(result.rows);
+    })
+    .catch(err => {
+      console.error(err);
+      response.status(500).json({ error: 'An unexpected error occured' });
+    });
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
