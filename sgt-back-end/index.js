042a37c5d25ff3d0834ed747d1b94fa0401f5188
Chris Gitmed <cgitmed@gmail.com>
2020-12-04 23:24:14 +0000

    Added JSON parsing middleware; Created POST route with error handling for bad requests

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -8,6 +8,10 @@ const db = new pg.Pool({
 
 const app = express();
 
+const JSONparser = express.json();
+
+app.use('/api/grades', JSONparser);
+
 app.get('/api/grades', (request, response) => {
   const sql = `
     select *
@@ -22,6 +26,19 @@ app.get('/api/grades', (request, response) => {
       console.error(err);
       response.status(500).json({ error: 'An unexpected error occured' });
     });
+});
+
+app.post('/api/grades', (request, response) => {
+  console.log('request.body:', request.body);
+  if (!request.body) {
+    response.status(400).json({ error: 'Enter fields (name, course, score)' });
+  } else if (!request.body.name ||
+    !request.body.course ||
+    !request.body.score ||
+    request.body.score < 1 ||
+    request.body.score > 100) {
+    response.status(400).json({ error: 'Enter all fields and a valid grade.' });
+  }
 });
 
 app.listen(3000, () => {
