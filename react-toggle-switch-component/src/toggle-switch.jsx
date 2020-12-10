cd8f4ad2126a35b4cb8de070833129f899a663db
Chris Gitmed <cgitmed@gmail.com>
2020-12-10 23:40:11 +0000

    Added an event listener to toggle the isOn property when the button is clicked

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class ToggleSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOn: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.isOn === true) {
+      this.setState({ isOn: false });
+    } else {
+      this.setState({ isOn: true });
+    }
+  }
+
   render() {
     return (
       <div className="border toggle-off">
-        <div className="circle"></div>
+        <div className="circle" onClick={this.handleClick}></div>
       </div>
     );
   }
