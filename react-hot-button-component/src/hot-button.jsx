738337feb3a79bd9da26eae419a23d9484351d23
Chris Gitmed <cgitmed@gmail.com>
2020-12-10 22:44:06 +0000

    Created variables for className and children; No more assigning to props.

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -13,24 +13,26 @@ class HotButton extends React.Component {
 
   render() {
     const clicks = this.state.clickCounter;
+    let className;
+    let children;
 
     if (clicks < 3) {
-      this.props.className = 'ice-button';
-      this.props.children = 'Ice Button';
+      className = 'ice-button';
+      children = 'Ice Button';
     } else if (clicks < 6) {
-      this.props.className = 'cold-button';
-      this.props.children = 'Cold Button';
+      className = 'cold-button';
+      children = 'Cold Button';
     } else if (clicks < 9) {
-      this.props.className = 'warm-button';
-      this.props.children = 'Warm Button';
+      className = 'warm-button';
+      children = 'Warm Button';
     } else if (clicks < 12) {
-      this.props.className = 'hot-button';
-      this.props.children = 'Hot Button';
+      className = 'hot-button';
+      children = 'Hot Button';
     } else {
-      this.props.className = 'nuclear-button';
-      this.props.children = 'Nuclear Button';
+      className = 'nuclear-button';
+      children = 'Nuclear Button';
     }
-    return <button className={this.props.className} onClick={this.handleClick}>{this.props.children}</button>;
+    return <button className={className} onClick={this.handleClick}>{children}</button>;
   }
 }
 
