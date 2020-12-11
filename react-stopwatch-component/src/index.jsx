81ef119d2ca1a873dea4789b1b2a0cfc39051505
Chris Gitmed <cgitmed@gmail.com>
2020-12-11 18:52:11 +0000

    Almost there; Switching to a new task for now

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -4,17 +4,38 @@ import ReactDOM from 'react-dom';
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentTime: 0 };
+    this.state = {
+      currentTime: 0,
+      isActive: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    let intervalID;
+    if (!this.state.isActive) {
+      this.setState({ isActive: true });
+      intervalID = setInterval(() => {
+        this.setState({ currentTime: this.state.currentTime + 1 });
+      }, 1000);
+    } else {
+      this.setState({ isActive: false });
+      clearInterval(intervalID);
+    }
   }
 
   render() {
+    let symbol = 'fa fa-play';
+    if (this.state.isActive) {
+      symbol = 'fa fa-pause';
+    }
     return (
       <div className="clock">
         <div className="circle">
           <h1>{this.state.currentTime}</h1>
         </div>
         <div>
-          <i className="fa fa-play" aria-hidden="true"></i>
+          <i className={symbol} aria-hidden="true" onClick={this.handleClick}></i>
         </div>
       </div>
     );
