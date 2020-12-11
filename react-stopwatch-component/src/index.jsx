bb1f9f85bedf73839922533114320e5431a5e4a2
Chris Gitmed <cgitmed@gmail.com>
2020-12-11 01:18:08 +0000

    Complete the basic structure of the Stopwatch component.

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -4,9 +4,19 @@ import ReactDOM from 'react-dom';
 class Stopwatch extends React.Component {
 
   render() {
-    return <p>placeholder</p>;
+    return (
+      <div className="clock">
+        <div className="row circle">
+          <h1>0</h1>
+        </div>
+        <div>
+          <p>symbol</p>
+        </div>
+      </div>
+    );
   }
 }
+
 ReactDOM.render(
   <Stopwatch/>,
   document.querySelector('#root')
