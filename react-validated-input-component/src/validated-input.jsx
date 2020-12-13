9d6648ecbbfbc780a88bd5788a1598b5c2736106
Chris Gitmed <cgitmed@gmail.com>
2020-12-13 22:22:27 +0000

    Imported an icon from linearIcons; Added style

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -7,6 +7,7 @@ class ValidatedInput extends React.Component {
       <div className="input-container">
         <label htmlFor="password" className="row">Password</label>
         <input type="password" name="password"/>
+        <span className="lnr lnr-cross-circle"></span>
       </div>
     );
   }
