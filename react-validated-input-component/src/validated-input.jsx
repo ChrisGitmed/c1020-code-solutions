50597728e65ec4ee67e19c6b61fe2470bb5d6618
Chris Gitmed <cgitmed@gmail.com>
2020-12-14 01:43:52 +0000

    Added constructor and handleChange methods to ValidatedInput class; Created state to hold password

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { password: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ password: event.target.value });
+  }
 
   render() {
     return (
       <div className="input-container">
         <label htmlFor="password" className="row">Password</label>
-        <input type="password" name="password"/>
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}/>
         <span className="lnr lnr-cross-circle"></span>
       </div>
     );
